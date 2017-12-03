ccf1f355d22c533979f9b9300172b46d7857e3f9
Chia-Hung Lin <qaz741012@gmail.com>
2017-12-03 19:43:53 +0800

    user can delete category with link on index page

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -4,20 +4,45 @@ class Admin::CategoriesController < ApplicationController
 
   def index
     @categories = Category.all
-    @category = Category.new
+
+    if params[:id]
+      @category = Category.find(params[:id])
+    else
+      @category = Category.new
+    end
   end
 
   def create
-    @categories = Category.all
     @category = Category.new(category_params)
 
     if @category.save
       flash[:notice] = "category was successfully created"
       redirect_to admin_categories_path
     else
+      @categories = Category.all
       flash.now[:alert] = "category was failed to create"
       render :index
     end
+  end
+
+  def update
+    @category = Category.find(params[:id])
+
+    if @category.update(category_params)
+      flash[:notice] = "category was successfully updated"
+      redirect_to admin_categories_path
+    else
+      @categories = Category.all
+      flash.now[:alert] = "category was failed to update"
+      render :index
+    end
+  end
+
+  def destroy
+    @category = Category.find(params[:id])
+    @category.destroy
+    flash[:notice] = "category was successfully deleted"
+    redirect_to admin_categories_path
   end
 
   private
