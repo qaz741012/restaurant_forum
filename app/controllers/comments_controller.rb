75b1a18c6a0c3353b95b4e40598c8f1be84bc6ef
Chia-Hung Lin <qaz741012@gmail.com>
2017-12-16 16:57:05 +0800

    add radio_button for score of comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -24,7 +24,7 @@ class CommentsController < ApplicationController
   end
 
   def comment_params
-    params.require(:comment).permit(:content)
+    params.require(:comment).permit(:content, :score)
   end
 
 end
