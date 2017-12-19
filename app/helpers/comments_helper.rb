bfd28015755340ae0fa14f4061c50e969cfaca3c
Chia-Hung Lin <qaz741012@gmail.com>
2017-12-19 21:10:01 +0800

    add average score of every restaurant

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -1,2 +1,13 @@
 module CommentsHelper
+  def average_score(restaurant)
+    sum = 0
+    if restaurant.comments.size > 0
+      restaurant.comments.each do |comment|
+        sum += comment.score
+      end
+      sum.to_f / restaurant.comments.size
+    else
+      0
+    end
+  end
 end
