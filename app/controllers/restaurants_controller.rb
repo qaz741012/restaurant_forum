2127000f0f31968a82a433d1a1e4487a660fec22
Chia-Hung Lin <qaz741012@gmail.com>
2017-12-26 22:33:41 +0800

    create dashboard action in restaurants controller

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -16,4 +16,8 @@ class RestaurantsController < ApplicationController
     @recent_comments = Comment.order(created_at: :desc).limit(10)
   end
 
+  def dashboard
+    @restaurant = Restaurant.find(params[:id])
+  end
+
 end
