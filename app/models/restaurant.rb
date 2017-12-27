a4a7af4d31eccce30a7a257473c649d5e2b720b5
Chia-Hung Lin <qaz741012@gmail.com>
2017-12-27 14:46:53 +0800

    create favorite model ,and setup relation among userm restaurant and favorite

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -6,4 +6,6 @@ class Restaurant < ApplicationRecord
 
   has_many :comments, dependent: :destroy
   has_many :users, through: :comments
+  has_many :favorites, dependent: :destroy
+  has_many :favorited_users, through: :favorites, source: :user
 end
