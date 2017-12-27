6a93aeb94a936a803bdf42470f41f0d8e5cb5242
Chia-Hung Lin <qaz741012@gmail.com>
2017-12-27 16:13:21 +0800

    add is_favorited? method to restaurant model for checking favorite/unfavorite button. add favorite / unfavorite button on index oage using shared partial

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -8,4 +8,9 @@ class Restaurant < ApplicationRecord
   has_many :users, through: :comments
   has_many :favorites, dependent: :destroy
   has_many :favorited_users, through: :favorites, source: :user
+
+  def is_favorited?(user)
+    self.favorited_users.include?(user)
+  end
+
 end
