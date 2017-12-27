4c6583b76393b1a8d708658690578b336f016eef
Chia-Hung Lin <qaz741012@gmail.com>
2017-12-27 16:33:29 +0800

    create like model, and add relation between user, like and restaurant

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -8,6 +8,8 @@ class Restaurant < ApplicationRecord
   has_many :users, through: :comments
   has_many :favorites, dependent: :destroy
   has_many :favorited_users, through: :favorites, source: :user
+  has_many :likes, dependent: :destroy
+  has_many :liked_users, through: :likes, source: :user
 
   def is_favorited?(user)
     self.favorited_users.include?(user)
