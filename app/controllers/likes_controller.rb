5f2b9cc8d2ed742abf76e20116abc7718f2191ad
Chia-Hung Lin <qaz741012@gmail.com>
2017-12-28 14:21:42 +0800

    make like/dislike function through create and destroy action in LikesController

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,11 +1,14 @@
 class LikesController < ApplicationController
 
   def create
-
+    like = Like.create!(restaurant_id: params[:restaurant_id], user: current_user)
+    redirect_back(fallback_location: root_path)
   end
 
   def destroy
-
+    like = Like.where(restaurant_id: params[:restaurant_id], user: current_user)
+    like.destroy_all
+    redirect_back(fallback_location: root_path)
   end
 
 end
