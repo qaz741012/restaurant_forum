70cd09cf3c6d57c15258b05f1145b7688542304b
Chia-Hung Lin <qaz741012@gmail.com>
2017-12-30 11:50:06 +0800

    create followship model, and setup self-referential relationships within users table through followships

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,11 +9,17 @@ class User < ApplicationRecord
   mount_uploader :avatar, AvatarUploader
 
   has_many :comments, dependent: :restrict_with_error
+
   has_many :restaurants, through: :comments
+
   has_many :favorites, dependent: :destroy
   has_many :favorited_restaurants, through: :favorites, source: :restaurant
+
   has_many :likes, dependent: :destroy
   has_many :liked_restaurants, through: :likes, source: :restaurant
+  
+  has_many :followships, dependent: :destroy
+  has_many :followings, through: :followships
 
   def admin?
     self.role == "admin"
